9237fb6db71e1d172873671451e262453ec1d8aa
geraldpipes <gerrypipes@hotmail.com>
2015-12-03 13:26:05 -0500

    more modular code woo

--- main.c
+++ main.c
@@ -1,68 +1,82 @@
+#define bint int
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
+
+
+SDL_Window *win = NULL;
+SDL_Surface *screen_surface = NULL;
+SDL_Surface *hello_surface = NULL;
+
+bint init() {
+  int success = 1;
+
+  if (SDL_Init(SDL_INIT_VIDEO)) {
+    printf("SDL_Init Error: %s\n", SDL_GetError());
+    success = 0;
+  } else {
+    win = SDL_CreateWindow("Hello World", SDL_WINDOWPOS_UNDEFINED,
+			   SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+    if (win == NULL) {
+      printf("SDL_CreateWindow Eroor: %s\n", SDL_GetError());
+      success = 1;
+    } else {
+      screen_surface = SDL_GetWindowSurface(win);
+    }
+    
+  }
+
+  return success;
+}
+
+bint load_media() {
+
+  int success = 1;
+
+  hello_surface = SDL_LoadBMP("hello.bmp");
+
+  if (hello_surface == NULL) {
+    printf("SDL_LoadBMP Error: %s\n", SDL_GetError());
+    success = 0;
+  }
+
+  return success;
+  
+}
+
+bint close() {
+
+  SDL_FreeSurface(hello_surface);
+  hello_surface = NULL;
+
+  SDL_DestroyWindow(win);
+  win = NULL;
+
+  SDL_Quit();
+
+}
+
 int main(int argc, char **argv) {
 
-  if(SDL_Init(SDL_INIT_VIDEO) != 0) {
-    printf("SDL_Init Error %s\n", SDL_GetError());
+  if (!init()) {
+    close();
+    printf("Failed to init\n");
     return 1;
   }
-  //create a window (win is a pointer to the struct)
-  SDL_Window *win = SDL_CreateWindow("Hello World", 100, 100,
-				     640, 480, SDL_WINDOW_SHOWN
-				               | SDL_WINDOW_FULLSCREEN);
-  if(win == NULL) {
-    printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
-    SDL_Quit();
+  if (!load_media()) {
+    close();
+    printf("Failed to load media\n");
     return 1;
+  } else {
+    //blitting imposes one surface on another
+    SDL_BlitSurface( hello_surface, NULL, screen_surface, NULL );
+    SDL_UpdateWindowSurface( win );
   }
-  //create a renderer
-  //ren is a pointer to it and it is registered with win
-  //used to draw on the window
-  SDL_Renderer *ren = SDL_CreateRenderer(win, -1,
-					 SDL_RENDERER_ACCELERATED |
-					 SDL_RENDERER_PRESENTVSYNC);
-  if(ren == NULL) {
-    SDL_DestroyWindow(win);
-    printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
-    SDL_Quit();
-    return 1;
-  }
+  SDL_Delay( 2000 );
 
-  //in order to load images you need to load it into memory
-  //then onto the rendering platform
+  close();
 
-  char *image_path = "./hello.bmp";
-  printf("test: image_path = %d\n", image_path);
-  
-  
-  SDL_Surface *bmp = SDL_LoadBMP(image_path);
-  if(bmp == NULL) {
-    SDL_DestroyRenderer(ren);
-    SDL_DestroyWindow(win);
-    printf("SDL_LoadBMP Error: %s\n", SDL_GetError());
-    SDL_Quit();
-    return 1;
-  }
-
-  SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, bmp);
-  SDL_FreeSurface(bmp);
-  if(tex == NULL) {
-    SDL_DestroyRenderer(ren);
-    SDL_DestroyWindow(win);
-    printf("SDL_CreateTextureFromSurface Error: %s\n", SDL_GetError());
-    SDL_Quit();
-    return 1;
-  }
-  for (int i = 0 ; i < 3 ; ++i) {
-    SDL_RenderClear(ren);
-    SDL_RenderCopy(ren, tex, NULL, NULL);
-    SDL_RenderPresent(ren);
-    SDL_Delay(10000);
-  }
-  SDL_DestroyTexture(tex);
-  SDL_DestroyRenderer(ren);
-  SDL_DestroyWindow(win);
-  SDL_Quit();
+  return 0;
 }
